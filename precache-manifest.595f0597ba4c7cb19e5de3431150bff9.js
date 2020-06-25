aa8a7cd57e9c08b483c6bf2e3dda0b53a26c0fbb
anuntachai <netumpire@gmail.com>
2020-06-25 23:01:29 +0700

    'deploy'

---
+++ precache-manifest.595f0597ba4c7cb19e5de3431150bff9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c8c31e84c1eab035f3b",
+    "url": "/thaichana/css/app.730e1f41.css"
+  },
+  {
+    "revision": "c15672ba131361e8e790",
+    "url": "/thaichana/css/chunk-vendors.421b32c5.css"
+  },
+  {
+    "revision": "e5305734ded76d6b3a176941e57be5af",
+    "url": "/thaichana/img/logo.e5305734.png"
+  },
+  {
+    "revision": "5a2fe800cec1207c28c71c181d593278",
+    "url": "/thaichana/index.html"
+  },
+  {
+    "revision": "e863c99a617efa0a2fca",
+    "url": "/thaichana/js/about.f4c2bdd1.js"
+  },
+  {
+    "revision": "9c8c31e84c1eab035f3b",
+    "url": "/thaichana/js/app.4830aa26.js"
+  },
+  {
+    "revision": "c15672ba131361e8e790",
+    "url": "/thaichana/js/chunk-vendors.d3178868.js"
+  },
+  {
+    "revision": "cc0c66aa0a70dad5dcb75645aa137195",
+    "url": "/thaichana/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/thaichana/robots.txt"
+  }
+]);
